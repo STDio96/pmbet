828854d47437835cc720b497806573367d607ea2
Sergo <sergo@M-Air-3.local>
2021-03-07 01:49:48 +0200

    changed to classes

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import convertDate from '../helpers/convertDate';
 import getIcon from '../helpers/descIcons';
 
-const WeatherCard = ({ cardData }) => {
+class WeatherCard extends React.Component {
+    render() {
+        let { cardData } = this.props;
+
+        return <li>
+            {cardData.windSpeed} {cardData.windDirection}
+            <img src={getIcon(cardData.description)} width='32' />
+            {cardData.description}
+            {cardData.pressure}: {convertDate(cardData.date)}
+        </li>
+    }
+}
+
+/* const WeatherCard = ({ cardData }) => {
     return <li>
         {cardData.windSpeed} {cardData.windDirection}
         <img src={getIcon(cardData.description)} width='32' />
         {cardData.description}
         {cardData.pressure}: {convertDate(cardData.date)}
     </li>
-}
+} */
 
 export default WeatherCard;
--- src/components/WeatherList.jsx
+++ src/components/WeatherList.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import WeatherCard from './WeatherCard';
 
-const WeatherList = ({ weatherData }) => (
+class WeatherList extends React.Component {
+    render() {
+        return <ul>
+            {this.props.weatherData.map((card) => {
+                return <WeatherCard key={card.id} cardData={card} />
+            })}
+        </ul>
+    }
+}
+
+/* const WeatherList = ({ weatherData }) => (
     <ul>
         {weatherData.map((card) => {
             return <WeatherCard key={card.id} cardData={card} />
         })}
     </ul>
-)
+) */
 
 export default WeatherList;
